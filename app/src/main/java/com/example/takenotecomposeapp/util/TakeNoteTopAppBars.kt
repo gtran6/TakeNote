6376fd2a142467eef97c4faae359c683b55372b9
gtran6 <gtran6@depaul.edu>
2025-03-07 12:18:13 -0600

    create TakeNoteTopAppBars with FilterTasksMenu

---
+++ app/src/main/java/com/example/takenotecomposeapp/util/TakeNoteTopAppBars.kt
@@ -0,0 +1,114 @@
+package com.example.takenotecomposeapp.util
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.takenotecomposeapp.MainTheme
+import com.example.takenotecomposeapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TakeNoteTopAppBars(
+    openDrawer: () -> Unit,
+    onFilterAllTasks: () -> Unit,
+    onFilterActiveTasks: () -> Unit,
+    onFilterCompletedTasks: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                textAlign = TextAlign.Left
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = openDrawer) {
+                Icon(Icons.Filled.Menu, stringResource(id = R.string.open_drawer))
+            }
+        },
+        actions = {
+            FilterTasksMenu(onFilterAllTasks, onFilterActiveTasks, onFilterCompletedTasks)
+        },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Preview
+@Composable
+private fun TakeNoteTopAppBarsPreview() {
+    MainTheme {
+        Surface {
+            TakeNoteTopAppBars( {}, {}, {}, {})
+        }
+    }
+}
+
+@Composable
+private fun TopAppBarDropdownMenu(
+    iconContent: @Composable () -> Unit,
+    content: @Composable ColumnScope.(() -> Unit) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.wrapContentSize(Alignment.TopEnd)) {
+        IconButton(onClick = { expanded = !expanded} ) { iconContent() }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.wrapContentSize(Alignment.TopEnd)
+        ) {
+            content { expanded = !expanded }
+        }
+    }
+}
+
+@Composable
+private fun FilterTasksMenu(
+    onFilterAllTasks: () -> Unit,
+    onFilterActiveTasks: () -> Unit,
+    onFilterCompletedTasks: () -> Unit
+) {
+    TopAppBarDropdownMenu(
+        iconContent = {
+            Icon(
+                painterResource(id = R.drawable.ic_filter_list),
+                stringResource(id = R.string.menu_filter)
+            )
+        }
+    ) { closeMenu ->
+        DropdownMenuItem(
+            onClick = { onFilterAllTasks; closeMenu() },
+            text = { Text(text = stringResource(id = R.string.nav_all)) }
+        )
+        DropdownMenuItem(
+            onClick = { onFilterActiveTasks; closeMenu() },
+            text = { Text(text = stringResource(id = R.string.nav_active)) }
+        )
+        DropdownMenuItem(
+            onClick = { onFilterCompletedTasks; closeMenu() },
+            text = { Text(text = stringResource(id = R.string.nav_completed)) }
+        )
+    }
+}
